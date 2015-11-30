f1ecd6c2e3d5760996680cdc3c9c56282418fdf7
amykangweb <selene6023@gmail.com>
2015-11-29 22:11:34 -0800

    Add unit tests for votes controller and service.

---
+++ spec/javascripts/services/votesService_spec.js
@@ -0,0 +1,22 @@
+describe('Service: votesService', function(){
+
+  beforeEach(module('quotes'));
+
+  var service, httpBackend;
+  var mockResults = {"motivational":[{"_id": {"$oid": "1"}, "body": "Motivational Quote.", "author": "Jane Doe", "category": "Motivational", "votes": [{"score": true}]}]};
+  var voteResults = [{"score": true}, {"score": true}];
+
+  beforeEach(inject(function(_votesService_, $httpBackend){
+    service = _votesService_;
+    httpBackend = $httpBackend;
+  }));
+
+  it('should create and return votes', function(){
+    var quote = mockResults.motivational[0];
+    httpBackend.expectPOST('api/quotes/' + quote._id['$oid'] + '/votes').respond(voteResults);
+    service.upVote(quote);
+    httpBackend.flush();
+    expect(quote.votes).toEqual(voteResults);
+  });
+
+});
